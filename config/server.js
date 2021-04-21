9e3e98874e944bc72360539b832c7b8ecb5659b8
Adin Hadrovic <adin.hadrovic@gmail.com>
2021-04-21 17:43:57 +0200

    no message

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e1ac2a172425bde17beed0b79b0211d0'),
+    },
+  },
+});
